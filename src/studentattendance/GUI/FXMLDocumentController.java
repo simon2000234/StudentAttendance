c93ba97d93b64a4b2374ec92c84d9db6e5a87936
andreas <andreas-cooper@hotmail.com>
2019-02-01 13:44:54 +0100

    andreas nothing

--- src/studentattendance/GUI/FXMLDocumentController.java
+++ src/studentattendance/GUI/FXMLDocumentController.java
@@ -29,7 +29,8 @@ public class FXMLDocumentController implements Initializable
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        // TODO
+       
+        
     }    
 
     @FXML
