68196648c7a4cae671b5fca3f03c65eae5c29a6a
CasperLC <43999061+CasperLC@users.noreply.github.com>
2019-02-01 13:36:34 +0100

    MainPage_start
    added:
     - Logged in as: Student labels
     - Present/Absent/Edit buttons
     - ImageView for picture of logged in
       student/teacher
     - Added PieChart for showing attendance
       percentage

---
+++ src/studentattendance/GUI/RootLayerController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentattendance.GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author Caspe
+ */
+public class RootLayerController implements Initializable
+{
+
+    @FXML
+    private Button btnPresent;
+    @FXML
+    private Button btnAbsent;
+    @FXML
+    private Button btnEdit;
+    @FXML
+    private PieChart chartPieChart;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        // TODO
+    }    
+    
+}
