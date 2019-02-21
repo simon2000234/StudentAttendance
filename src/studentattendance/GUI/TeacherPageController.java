772bdcece01ea8a94f9a4cae201fefc354446183
CasperLC <43999061+CasperLC@users.noreply.github.com>
2019-02-21 13:11:19 +0100

    Absentdays

--- src/studentattendance/GUI/TeacherPageController.java
+++ src/studentattendance/GUI/TeacherPageController.java
@@ -5,16 +5,26 @@
  */
 package studentattendance.GUI;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import studentattendance.BE.Attendance;
 import studentattendance.BE.Student;
 import studentattendance.BE.Teacher;
+import studentattendance.StudentAttendance;
 
 /**
  * FXML Controller class
@@ -23,7 +33,7 @@ import studentattendance.BE.Teacher;
  */
 public class TeacherPageController implements Initializable
 {
-    
+
     private SAModel model;
     @FXML
     private Label lblTeacherName;
@@ -34,6 +44,11 @@ public class TeacherPageController implements Initializable
     private TableColumn<Student, String> columnStudentName;
     @FXML
     private TableColumn<Student, Double> columnAttendance;
+    private int Mandag;
+    private int Tirsdag;
+    private int Onsdag;
+    private int Torsdag;
+    private int Fredag;
 
     /**
      * Initializes the controller class.
@@ -43,7 +58,7 @@ public class TeacherPageController implements Initializable
     {
         columnStudentName.setCellValueFactory(new PropertyValueFactory<>("name"));
         columnAttendance.setCellValueFactory(new PropertyValueFactory<>("attendanceString"));
-        
+
     }
 
     public void setMsmodel(SAModel model)
@@ -54,11 +69,63 @@ public class TeacherPageController implements Initializable
         tableAttendance.setItems(model.getStudents());
         columnAttendance.setComparator(columnAttendance.getComparator().reversed());
         tableAttendance.getSortOrder().add(columnAttendance);
-        
-//        for (Student student : model.getStudents())
-//        {
-//            System.out.println(""+student.);
-//        }
+
     }
-    
+
+    @FXML
+    private void close(ActionEvent event)
+    {
+        try
+        {
+            Parent root;
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(StudentAttendance.class.getResource("GUI/FXMLDocument.fxml"));
+            root = loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Log in");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+            Stage current = (Stage) lblTeacherName.getScene().getWindow();
+            current.close();
+        } catch (IOException ex)
+        {
+            Logger.getLogger(RootLayerController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void absentDays(Student student)
+    {
+        Mandag = 0;
+        Tirsdag = 0;
+        Onsdag = 0;
+        Torsdag = 0;
+        Fredag = 0;
+        
+            for (Attendance attendance : student.getAttendance())
+            {
+                if (attendance.getDayOfWeek().contains("Mandag") && attendance.isIsAttending()==false)
+                {
+                    Mandag++;
+                }
+                if (attendance.getDayOfWeek().contains("Tirsdag") && attendance.isIsAttending()==false)
+                {
+                    Tirsdag++;
+                }
+                if (attendance.getDayOfWeek().contains("Onsdag") && attendance.isIsAttending()==false)
+                {
+                    Onsdag++;
+                }
+                if (attendance.getDayOfWeek().contains("Torsdag") && attendance.isIsAttending()==false)
+                {
+                    Torsdag++;
+                }
+                if (attendance.getDayOfWeek().contains("Fredag") && attendance.isIsAttending()==false)
+                {
+                    Fredag++;
+                }
+            }
+            
+    }
+
 }
